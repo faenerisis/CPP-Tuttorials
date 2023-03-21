4cc7e5cc102654983e3f46a48bb18ba7177a5347
faenerisis <67673597+faenerisis@users.noreply.github.com>
2023-03-21 08:33:41 +0300

    Create example-1.cpp

---
+++ example-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() { 
+     //Decleration of the variable
+    int num_1;
+    std::cout<<"Enter your favorite number between 1 and 100:";
+    //Geting the value of num_1
+    std::cin>>num_1;
+    //Printing the new value of num_1
+    std::cout<<num_1;
+    //Adding new line so we can clearly see the value of num_1
+    std::cout<<std::endl;
+}
