c718ce60b483cfca1763bf1e8f5466711217ea81
faenerisis <67673597+faenerisis@users.noreply.github.com>
2023-03-25 17:20:37 +0300

    Create example-7.cpp

---
+++ example-7.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+#include <string>
+using std::string;
+
+#include <vector>
+using std::vector;
+using std::begin;
+using std::end;
+
+#include <algorithm>
+using std::sort;
+using std::count;
+
+int main() {
+    vector<int> nums;
+    
+    for (int i = 0; i < 10; i++) {
+        nums.push_back(i);
+        cout << nums[i] << " \n";
+    }
+    
+    for (auto item : nums) {
+        cout << item << " ";
+    }
+    cout << "\n";
+    
+    vector<string> words;
+
+    for (int i = 0; i < 3; i++) {
+        string s;
+        if (i != 3) {
+            cout << i + 1 << " enter a word: ";
+            cin >> s;
+            words.push_back(s);
+        }
+    }
+    
+    for (auto item : words) {
+        cout << item << " ";
+    }
+    cout << "\n";
+    return 0;
+}
